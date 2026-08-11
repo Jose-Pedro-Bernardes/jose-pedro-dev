f88d98aaaf84bf21d6b5d45de906d8c970d26d9a
Jose Pedro Bernardes <josepedro.bernardes@gmail.com>
2026-08-11 01:19:00 -0300

    chore(fonts): add dm sans font setup

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,10 @@
-import { Montserrat } from "next/font/google";
+import { Montserrat, DM_Sans } from "next/font/google";
+
+export const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400"], // Regular
+  variable: "--font-dm-sans",
+});
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
