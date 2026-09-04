86d1ef0537517bb41089f0d6ca216bd11ec1411c
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-09-04 12:31:52 -0300

    chore: add a title to test some feature

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,7 +1,7 @@
 export function AboutMe() {
     return (
-        <section className="h-[500px]">
-
+        <section className="h-[700px] w-full flex">
+            <h2>But who is behind the code?</h2>
         </section>
     )
 }
