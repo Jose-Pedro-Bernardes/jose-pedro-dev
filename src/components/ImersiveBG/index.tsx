c35f139647473b84e62eb41a12c5ee2ddfa6c1d3
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-09-05 22:52:07 -0300

    feat: update ImersiveBG component with new EULA image and adjust opacity settings

--- src/components/ImersiveBG/index.tsx
+++ src/components/ImersiveBG/index.tsx
@@ -14,7 +14,6 @@ export function ImersiveBG() {
         min-[300px]:max-[720px]:w-[100%]
         aspect-square
 
-        opacity-[var(--bg-Opacity)]
         -z-10
 
         max-[450px]:top-[30%]
@@ -29,14 +28,12 @@ export function ImersiveBG() {
     >
       <div
         className="
-          relative
-          w-full
-          h-full
-          max-w-full
-          max-h-full
+          absolute
+          inset-0
           rounded-full
           overflow-hidden
           animate-spin-slow
+          opacity-50
         "
         style={{
           maskImage:
@@ -61,6 +58,43 @@ export function ImersiveBG() {
           loading="eager"
         />
       </div>
+
+      <div
+        className="
+          absolute
+          inset-0
+          flex
+          items-center
+          justify-center
+          rounded-full
+          overflow-hidden
+        "
+        style={{
+          maskImage:
+            "radial-gradient(circle, black 50%, transparent 100%)",
+          WebkitMaskImage:
+            "radial-gradient(circle, black 50%, transparent 100%)",
+        }}
+      >
+        <Image
+          src="/assets/eula.png"
+          width={700}
+          height={700}
+          alt="José Pedro Bernardes"
+          className="
+            opacity-90
+            border-[var(--accent1)]/20
+            border-[4px]
+            w-[65%]
+            h-[65%]
+            rounded-full
+            object-cover
+            pointer-events-none
+            select-none
+          "
+        />
+      </div>
     </aside>
   );
 }
+
