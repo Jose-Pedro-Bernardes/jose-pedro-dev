8b644f29677abe2f14a39c4fc1e084a0b6a3460a
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-09-17 14:20:00 -0300

    feat: create How I Work page with Initial content

---
+++ src/app/how-i-work/page.tsx
@@ -0,0 +1,29 @@
+import {Header} from "@/components/Header";
+
+export default function HowIWork() {
+
+    return (
+        <div 
+          className="
+            bg-[image:var(--bg-primary)] 
+            h-screen
+          "
+        >
+            <Header></Header>
+            <div 
+              className="
+                w-full 
+                h-full 
+                flex 
+                flex-col 
+                items-center 
+                justify-center 
+                gap-8
+               "
+            >
+                <h1 className="text-4xl font-bold">How I Work</h1>
+                <p>This is the how I work page.</p>
+            </div>
+        </div>
+    );
+}
