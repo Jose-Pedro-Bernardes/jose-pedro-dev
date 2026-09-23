387eb1355d6d0d530e027e03c8de0e49174e4c9e
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-09-23 12:48:35 -0300

    chore(how-i-work Page): update placeholder text to indicate page is under construction

--- src/components/how-i-work/HeroContent/index.tsx
+++ src/components/how-i-work/HeroContent/index.tsx
@@ -13,7 +13,7 @@ export function HeroContent() {
           "
         >
             <h1 className="text-4xl font-bold">How I Work</h1>
-            <p>This is the how I work page.</p>
+            <p>This page is currently under construction.</p>
 
         </div>
         
